a4144926a69a7a2c93c6c0618fac27d0da3613cd
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-07-17 20:57:13 +0600

    database connected and load menu data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,8 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//mongodb
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7em2cfy.mongodb.net/?retryWrites=true&w=majority`;
+// new mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ek7mjck.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,14 +24,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const usersCollection = client.db("bistroDB").collection("users");
-    const menuCollection = client.db("bistroDB").collection("menu");
-    const reviewsCollection = client.db("bistroDB").collection("reviews");
-    const cartCollection = client.db("bistroDB").collection("carts");
-    //all database code start from here
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // const usersCollection = client.db("BistroBoss").collection("users");
+    const menuCollection = client.db("BistroBoss").collection("menu");
+    const reviewsCollection = client.db("BistroBoss").collection("reviews");
+    // const cartCollection = client.db("BistroBoss").collection("carts");
 
-    //all database code ends from here
+    // database crud operation routes
+    // get menu data
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
 
+    // get reviews data
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,6 +50,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 run().catch(console.dir);
