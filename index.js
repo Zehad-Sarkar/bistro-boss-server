f395fb9a0fa30fd778323f097e08552fdaa14d77
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-07-26 15:55:51 +0600

    jwt api create and set with user authprovider

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ async function run() {
 
     // database crud operation routes
 
+    //jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
     //get user related api get user from database
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
